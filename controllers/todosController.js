dd6c385e85938b286adbeecccfde570765cb3d8b
D.P. Hurley <hurley.dannyp@gmail.com>
2017-05-16 16:03:51 -0400

    Updated Todos routes to actually run.

--- controllers/todosController.js
+++ controllers/todosController.js
@@ -3,15 +3,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-    //do stuff
+	console.log('getting all of the todos');
+	res.send('All of the todos!');
 });
 
 router.get('/:id', function(req, res){
-    //do stuff
+   	console.log('getting one todo');
+	res.send('Getting todo with the ID of ' + req.params.id); 
 });
 
 router.get('/:id/edit', function(req, res){
-    //do stuff
+    console.log('editing one todo');
+	res.send('Editing todo with the ID of ' + req.params.id);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 var express = require('express');
+
+var todosController = require('./controllers/todosController.js');
+var userProfileController = require('./controllers/userProfileController.js')
+
+var app = express();
+
+app.use('/todos', todosController);
+app.use('/userProfile', userProfileController);
+
+app.listen(3000, function () {
+	console.log('Server is running on port 3000!');
+});
