e1506118403eeec5999affde8a2b76ef267a6904
D.P. Hurley <hurley.dannyp@gmail.com>
2017-05-16 16:09:01 -0400

    user profile controller actually works

--- controllers/userProfileController.js
+++ controllers/userProfileController.js
@@ -2,20 +2,19 @@ var express = require('express');
 
 var userProfileRouter = express.Router();
 
-userProfileRouter.get('/userProfile', function(req, res){
-    //do stuff
+userProfileRouter.get('/', function(req, res){
+	console.log('getting all of the User Profiles');
+	res.send('All of the User Profiles!');
 });
 
-userProfileRouter.get('/userProfile/:id', function(req, res){
-    //do stuff
+userProfileRouter.get('/:id', function(req, res){
+   	console.log('getting one User Profile');
+	res.send('Getting User Profile with the ID of ' + req.params.id); 
 });
 
-userProfileRouter.get('/userProfile/:id/edit', function(req, res){
-    //do stuff
+userProfileRouter.get('/:id/edit', function(req, res){
+    console.log('editing one User Profile');
+	res.send('Editing User Profile with the ID of ' + req.params.id);
 });
-
-function doThings() {
-	console.log('doing things');
-}
 
 module.exports = userProfileRouter;
